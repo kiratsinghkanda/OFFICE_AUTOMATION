a38ac0e08d98b1cb896e043a79281208d6110554
kiratsinghkanda <119124210+kiratsinghkanda@users.noreply.github.com>
2024-12-25 22:42:33 +0530

    Update Extract pdf from Website.py

--- Extract pdf from Website.py
+++ Extract pdf from Website.py
@@ -85,13 +85,12 @@ def extract_url_pdf(input_url,folder_path=os.getcwd()):
 # In[5]:
 
 
-extract_url_pdf(input_url="https://www.icai.org/post.html?post_id=17843")
+extract_url_pdf(input_url="https://www.physicsandmathstutor.com/physics-revision/gcse-edexcel/")
 
 
 # In[6]:
 
 
-extract_url_pdf(input_url="https://cbic-gst.gov.in/central-tax-notifications.html")
 
 # https://cbic-gst.gov.in/central-tax-notifications.html
 
